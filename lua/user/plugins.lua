6c002072f3eee91ade89eb942f817caa9ecf0f5a
ziotonak <ziotonak@gmail.com>
2024-03-24 23:58:54 +0100

    Add neorg plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -153,5 +153,30 @@ require("lazy").setup({
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         end
+    },
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true
+    },
+    {
+        "nvim-neorg/neorg",
+        dependencies = { "luarocks.nvim" },
+        lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "*", -- Pin Neorg to the latest stable release
+        opts = {
+            load = {
+                ["core.defaults"] = {},  -- Loads default behaviour
+                ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.dirman"] = {      -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes"
+                        },
+                        default_workspace = "notes"
+                    }
+                }
+            }
+        }
     }
 })
